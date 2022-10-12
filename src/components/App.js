eecd1e6c1334183216da43d2ea6ddd39acaa0ac0
Hafizur Rahman <83756518+hafeez25@users.noreply.github.com>
2022-10-12 09:58:08 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  return (
+    <div id="main">
+      <div className="date-time"></div>
+    </div>
+  )
+}
+
+
+export default App;
