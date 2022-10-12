896238f0974370975dc1163b0fd4e4cdfb34d610
Hafizur Rahman <99hafizurrahman@gmail.com>
2022-10-12 10:38:32 +0530

    clock add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
+  const [dateTime, setDateTime] = useState(
+    new Date().toLocaleDateString() + ", " + new Date().toLocaleTimeString()
+  );
+
+  setInterval(() => {
+    setDateTime(
+      new Date().toLocaleDateString() + ", " + new Date().toLocaleTimeString()
+    );
+  }, 1000);
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{dateTime}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
